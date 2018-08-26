b25ccd93f043fdf17d94d758ed10ee34619f6639
Jim Chen <18219111672@163.com>
2018-08-26 16:16:14 +0000

    Apply fixes from StyleCI

--- src/Exceptions/Exception.php
+++ src/Exceptions/Exception.php
@@ -5,7 +5,8 @@
  *
  * (c) JimChen <18219111672@163.com>
  *
- * This source file is subject to the MIT license that is bundled.
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
  */
 
 namespace JimChen\Weather\Exceptions;
